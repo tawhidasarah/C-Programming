f75f835bcfd591bf79aa5d908bd024ade02536a1
tawhidasarah <tawhidasarah32@gmail.com>
2026-07-08 22:04:24 +0600

    Create storeRoll&Quesmarksby4Students.c

---
+++ 2D Array/storeRoll&Quesmarksby4Students.c
@@ -0,0 +1,24 @@
+//Ques: Write a program to store roll number and Ques marks obtained by 4 students side by side in a matrix. 
+#include<stdio.h>
+int main()
+{
+    int r;
+    printf("enter row number:");
+    scanf("%d",&r);
+    int c;
+    printf("enter coloumn number:");
+    scanf("%d",&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+        scanf("%d",&arr[i][j]);
+        }
+    }
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+        printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
